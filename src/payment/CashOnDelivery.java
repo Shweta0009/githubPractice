11ab6fd13f201f9cb893d594b4ce8436a99d467c
Shweta0009 <shwetarai052004@gmail.com>
2023-12-03 12:23:19 +0530

    secound commit

---
+++ src/payment/CashOnDelivery.java
@@ -0,0 +1,9 @@
+package payment;
+
+public class CashOnDelivery {
+
+	public static void main(String[] args) {
+System.out.println("order is placed");
+	}
+
+}
---
+++ src/paymentText/CashOnDeliveryText.java
@@ -0,0 +1,12 @@
+package paymentText;
+
+class CashOnDeliveryText {
+
+	
+
+	public void m1(String payple) {
+		System.out.println("paple is accepted");
+
+	}
+
+}
