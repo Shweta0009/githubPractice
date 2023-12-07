1e1d9c0cf036d706d4e22e6ba09d7003b16caecc
Shweta0009 <shwetarai052004@gmail.com>
2023-12-07 18:55:40 +0530

    new changes

--- src/payment/order.java
+++ src/payment/order.java
@@ -3,8 +3,7 @@ package payment;
 public class order {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+String s= "t-shirt";
 	}
 
 }
--- src/paymentText/OrderTest.java
+++ src/paymentText/OrderTest.java
@@ -3,7 +3,8 @@ package paymentText;
 public class OrderTest {
 
 	public static void main(String[] args) {
-int n= 20;
+int n= 20; 
+String city="lucknow";
 	}
 
 }
